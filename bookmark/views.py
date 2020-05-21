994e911c9866fb07d4916fa39a3d8cc9bb760c4f
KangEunseo <s2018w21@e-mirim.hs.kr>
2020-05-21 10:16:38 +0900

    templates bookmark_create

--- bookmark/views.py
+++ bookmark/views.py
@@ -9,8 +9,8 @@ class BookmarkList(ListView):  # bookmark_list.html
     model = Bookmark
 
 
-class BookmarkCreateView(CreateView):  # bookmark_form.html
+class BookmarkCreateView(CreateView):  # bookmark_form.html - form 이름이 흔해서 아래처럼 create로 바꿔준 거
     model = Bookmark
-    fields = ['site_name', 'url']
-    template_name_suffix = '_create'  # bookmark_create.html
+    fields = ['site_name', 'url']      # <form> -> fields
+    template_name_suffix = '_create'   # bookmark_create.html
     success_url = reverse_lazy('bookmark:list')
